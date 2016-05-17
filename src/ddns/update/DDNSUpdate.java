d54c8abe792b204ea50bdc6de1b5830dedd43aa8
JamesSaunders1989 <james.saunders1989@gmail.com>
2016-05-17 11:59:30 +0100

    DNS Updater

---
+++ src/ddns/update/DDNSUpdate.java
@@ -0,0 +1,32 @@
+
+package ddns.update;
+
+import java.net.*;
+import java.io.*;
+
+/**
+ *
+ * @author james
+ */
+public class DDNSUpdate {
+
+
+    public static void main(String[] args) {
+        
+    }
+    
+    public void getIP()
+    {
+        URL whatismyip = new URL("http://checkip.amazonaws.com");
+    }
+    
+    
+    public void setIP()
+    {
+        
+    }
+    
+    
+    
+    
+}
