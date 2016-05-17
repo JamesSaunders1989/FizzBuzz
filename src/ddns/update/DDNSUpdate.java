65575cf1c8d40d7113008fe42253a7d222792285
JamesSaunders1989 <james.saunders1989@gmail.com>
2016-05-17 13:37:31 +0100

    DNS Updater

--- src/ddns/update/DDNSUpdate.java
+++ src/ddns/update/DDNSUpdate.java
@@ -15,10 +15,38 @@ public class DDNSUpdate {
         
     }
     
-    public void getIP()
+    public String getIP()
     {
+        try
+        {
         URL whatismyip = new URL("http://checkip.amazonaws.com");
+        BufferedReader in = new BufferedReader(new InputStreamReader(whatismyip.openStream()));
+        
+        String ip = in.readLine();
+        return ip;
+        }
+        catch(Exception e)
+        {
+            return e.toString();
+        }
+                
     }
+    
+    public String getSiteIP()
+    {
+        try
+        {
+        InetAddress getAddress = java.net.InetAddress.getByName("mrsaladbowl.com");
+        String address = getAddress.getHostAddress();
+        return address;
+        }
+        catch(Exception e)
+        {
+            return e.toString();
+        }
+    }
+                
+    
     
     
     public void setIP()
