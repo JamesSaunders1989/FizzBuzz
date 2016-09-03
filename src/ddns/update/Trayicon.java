930a23e7732a655d18d15f89ea2178e45d1c83a4
James Saunders <james.saunders1989@gmail.com>
2016-09-03 20:49:06 +0100

    fully functional with tray icon and menu

--- src/ddns/update/Trayicon.java
+++ src/ddns/update/Trayicon.java
@@ -26,13 +26,23 @@ public class Trayicon {
    
     public static TrayIcon trayIcon;
     
+    public Interface interfacey;
+    public boolean booleanstart;
     
-    public static void main(String[] args)
+    CheckboxMenuItem cbi = new CheckboxMenuItem("Auto Start");
+//    public static void main(String[] args)
+//    {
+//        Trayicon trayloader = new Trayicon();
+//        trayloader.addTray();
+//    }
+//    
+    
+    
+    public Trayicon(Interface int1)
     {
-        Trayicon trayloader = new Trayicon();
-        trayloader.addTray();
+        interfacey = int1;
+        addTray();
     }
-    
     
     public void addTray()
     {
@@ -50,33 +60,61 @@ public class Trayicon {
 //        TrayIcon trayIcon = new TrayIcon(trayIconImage.getScaledInstance(trayIconWidth, -1, Image.SCALE_SMOOTH));
 
         Image icon = Toolkit.getDefaultToolkit().getImage("C:/Users/Salad/Documents/NetBeans/DNS-updater/src/Images/DDNSUPsmall.png");
+        
         trayIcon = new java.awt.TrayIcon(icon, "DDNS Update");
+        
+        trayIcon.setImageAutoSize(true);
         
         PopupMenu menu = new PopupMenu();
         trayIcon.setPopupMenu(menu);
         
         
-        
-        MenuItem aboutItem = new MenuItem("About");
+        MenuItem show = new MenuItem("Show");
         MenuItem exit = new MenuItem("Exit");
         
-        CheckboxMenuItem cbi = new CheckboxMenuItem("Auto Start");
         
-        menu.add(aboutItem);
         
+        
+        
+        
+        menu.add(show);
         menu.add(cbi);
         menu.addSeparator();
         menu.add(exit);
         
-//        ActionListener exitListener;
-//        exitListener = new ActionListener() {
-//            public void ActionPerformed(ActionEvent e)
-//            {
-//                System.exit(0);
-//            }
-//        };
-//        exit.addActionListener(exitListener);
-                
+
+        
+        
+        ActionListener exitListener = new ActionListener() {
+            public void actionPerformed(ActionEvent e)
+            {
+                System.exit(0);
+            }
+        };
+        exit.addActionListener(exitListener);
+          
+        
+        show.addActionListener(new ActionListener() {
+            
+            public void actionPerformed(ActionEvent e)
+            {
+                interfacey.setVisible(true);
+                boolean boo1 = interfacey.con.getProp("Startup").equalsIgnoreCase("true");
+                interfacey.setjCheckboxSet(boo1);
+            }
+        });
+        
+        cbi.addActionListener(new ActionListener() {
+            
+            public void actionPerformed(ActionEvent e)
+            {
+                interfacey.boostart = cbi.getState();
+                interfacey.runonstart = "" + cbi.getState();
+                interfacey.setjCheckboxSet(cbi.getState());
+                interfacey.con.saveProp("Startup", "" + cbi.getState());
+            }
+        });
+        
         try
         {
             SystemTray tray = SystemTray.getSystemTray();
@@ -87,6 +125,13 @@ public class Trayicon {
             error.printStackTrace();
         }
         
+    }
+    
+    
+    public void setcheckbox(boolean boostart)
+    {
+        booleanstart = boostart;
+        cbi.setState(boostart);
     }
     
     
